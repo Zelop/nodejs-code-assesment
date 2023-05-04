bb2581d23a2618e8654b88fe7b9b2a3435453074
Zelop <zelopdotnet@proton.me>
2023-05-04 17:05:01 +0200

    Added utils and express functions.

--- main.js
+++ main.js
@@ -12,6 +12,8 @@ const port = 8000;
     We are going to store the lists from mocky into variables.
     The proper way would've been having both mocky lists on a database
     and querying the DB requesting for the specific data we need.
+    I am unsure if by "thinking some ways" I am supposed to in fact implement one 
+    and store the data from the urls on it. 
 */
 
 // Urls where the data is stored
@@ -20,6 +22,7 @@ const URLPolicies = "https://www.mocky.io/v2/580891a4100000e8242b75c5"
 
 
 async function main() {
+    console.warn("Attention! Role filtering isn't enabled yet. Do NOT use in Production"); // TODO Finish and remove
     let CLIENTS, POLICIES;
     // We start by loading the data into vars.
     try {
@@ -37,21 +40,35 @@ async function main() {
         return;
     }
 
-// TODO Express GET functions //
+    // We proceed by declaring express GET functions
+    // TODO Express GET functions //
+    // PENDING ROLE AUTHENTICATION //
 
-// Filter by user_id
-// requires roles: 'users', 'admin'
+    // Filter by user_id
+    // requires roles: 'users', 'admin'
+    app.get('/filter/id/:id', (req, res) => {
+        res.json(utils.get_user_by_id(req.params.id, CLIENTS));
+    });
 
-// Filter by username
-// requires roles: 'users', 'admin'
+    // Filter by username
+    // requires roles: 'users', 'admin'
+    app.get('/filter/username/:username', (req, res) => {
+        res.json(utils.get_user_by_name(req.params.username, CLIENTS));
+    });
 
-// Get list policies
-// requires roles: 'admin'
+    // Get list policies linked to user
+    // requires roles: 'admin'
+    app.get('/policies/list/:username', (req, res) => {
+        res.json(utils.get_policies_from_user(req.params.username, POLICIES, CLIENTS));
+    });
 
-// Get user linked to policy
-// requires roles: 'admin'
+    // Get user linked to policy
+    // requires roles: 'admin'
+    app.get('/policies/users/:id', (req, res) => {
+        res.json(utils.get_user_from_policy(req.params.id, POLICIES, CLIENTS));
+    });
 
-//We start the express server
+    // We start the express server
     app.listen(port, () => {
         console.log(`Express listening on port ${port}.`)
     })
--- utils.js
+++ utils.js
@@ -1,3 +1,7 @@
+/*
+Roberto López (zelopdotnet@proton.net)
+*/
+
 const https = require('https');
 
 // We are using https for the requests so we require the minimum on external libraries.
@@ -26,10 +30,37 @@ get_url: function(url) { // Queries url and returns the response
 }
 )},
 
-get_user: function(userid, userlist){
-    // Return user, or empty if user doesn't exist.
-    // TODO /CHECK, probably just return user_role 
-    return userlist.filter(function(item) {
+
+// We should be querying a database. I'm making the assumption every user and policy id is unique and there are no duplicates.
+// So I will be using .find() instead of .filter() (except on get_policies_from_user)
+
+get_user_by_id: function(userid, userlist){
+    // Return user, or undefined if id doesn't exist.
+    return userlist.find(function(item) {
         return item.id == userid;
+    })
+},
+
+get_user_by_name: function(name, userlist){
+    // Return user, or undefined if name doesn't exist.
+    return userlist.find(function(item) {
+        return item.name == name;
+    })
+},
+
+get_user_from_policy: function(id, policies, userlist){
+    // We find the policy, if any.
+    policy = policies.find(function(item) {
+        return item.id == id;
+    })
+    // We then return the linked user, if any.
+    return this.get_user_by_id(policy.clientId, userlist);
+},
+
+get_policies_from_user: function(name, policies, userlist){
+    user = this.get_user_by_name(name, userlist);
+    
+    return policies.filter(function(item) {
+        return item.clientId == user.id;
     })
 }}
